c8b6ef8fd47e7321b3ee6ae610f2baad20984b4a
Ashish <ashish.narkhede90@gmail.com>
2020-05-10 16:04:20 -0700

    Added babel for transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,20 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // transform all js files
+                exclude: /(node_modules)/, // optional param -  exclude files in node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    } // use babel-loader for transformation
+                }
+            }
+        ]
     }
 }
 
